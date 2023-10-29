e3ebef901094fc7275b4fd7ecf717bd1975bee3d
Daniel Quintero <dag@dagPC>
2023-10-29 15:04:52 -0400

    remove build dir from version control

--- src/build/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dag/src/BezierCurve/src/glm
-# Build directory: /home/dag/src/BezierCurve/src/build/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- src/build/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dag/src/BezierCurve/src/glm/glm
-# Build directory: /home/dag/src/BezierCurve/src/build/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
